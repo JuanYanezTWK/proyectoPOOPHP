db6c3ad12dcf01375e041c7b877c078731a5c492
JuanYanezTWK <jcarlosyz@gmail.com>
2018-06-28 18:45:30 -0400

    project php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<html lang="es">
+    <head>
+        <title>
+        Usuarios
+        </title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/fondo.css">
+        <link href="themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
+	    <link href="Scripts/jtable/themes/metro/green/jtable.css" rel="stylesheet" type="text/css" />
+	
+	    <script src="scripts/jquery-1.6.4.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
+        <script src="Scripts/jtable/jquery.jtable.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <form id="formulario">
+        <div id="usuarios" style="width: 700px;border:2px solid black; margin:auto"></div>       
+        </form>
+    </body>
+</html>
+
+<script src = "tab.js" type="text/javascript"> </script>
